dd0a38ff8e50436b33d37f03ece33bebb0f41ffe
Léo Desktop <peyronnet.leo@gmail.com>
2022-12-01 11:12:36 +0100

    cezarAffine (non-fini)

--- tp1.py
+++ tp1.py
@@ -13,9 +13,10 @@ def codeCezar(alphabet,mot,cle):
             if y==len(alphabet):
                 print("attention: un des caractères du mot n'est pas dans l'alphabet !")
                 exit()
+
         y+=cle
-        if y>=len(alphabet):
-            y=y%len(alphabet)
+        y=y%len(alphabet)
+
         result+=alphabet[y]
     return result
 def decodeCezar(alphabet,mot,cle):
@@ -25,9 +26,35 @@ def decodeCezar(alphabet,mot,cle):
         y=0
         while (mot[i]!=alphabet[y]):
             y+=1
-        result+=alphabet[y-cle]
+        y-=cle
+        result+=alphabet[y]
     return result
 
+def codeCezarAff(alphabet,mot,a,b):
+    result=""
+    for i in range(len(mot)):
+        y=0
+        while (mot[i]!=alphabet[y]):
+            y+=1
+            if y==len(alphabet):
+                print("attention: un des caractères du mot n'est pas dans l'alphabet !")
+                exit()
+
+        y=a*y+b
+        y=y%len(alphabet)
+
+        result+=alphabet[y]
+    return result
+def decodeCezarAff(alphabet,mot,a,b):
+    result=""
+    for i in range(len(mot)):
+        y=0
+        while (mot[i]!=alphabet[y]):
+            y+=1
+        y=a*(y-b)
+        y=y%len(alphabet)
+        result+=alphabet[y]
+    return result
 a=["""!"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_'abcdefghijklmnopqrstuvwxyz{é}£ ê""",'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
 m=["Si deux hommes ont la même opinion. L'un d'eux est de trop","OUI"]
 
@@ -35,6 +62,7 @@ while(1):
     print("=========CHOIX DE LA METHODE===========")
     print("1. Code Inverse")
     print("2. Code Cézar")
+    print("3. Code Cézar Affine")
     print("q. Quitter")
     choix=input("choix?:")
     if choix=="1":
@@ -54,6 +82,16 @@ while(1):
         m[i]=codeCezar(a[y],m[i],c)
         print("Mot codé:",m[i])
         m[i]=decodeCezar(a[y],m[i],c)
+        print("Mot décodé:",m[i])
+    elif choix=="3":
+        print("=========CODE CEZAR AFFINE===========")
+        y=int(input("Choix de l'alphabet:"))
+        i=int(input("Choix du mot:"))
+        ca=int(input("Choix du coefficient du polynôme:"))
+        cb=int(input("Choix du décalage du polynôme:"))
+        m[i]=codeCezarAff(a[y],m[i],ca,cb)
+        print("Mot codé:",m[i])
+        m[i]=decodeCezarAff(a[y],m[i],ca,cb)
         print("Mot décodé:",m[i])
     elif choix=="q":
         exit()
